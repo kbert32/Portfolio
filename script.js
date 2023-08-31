ebd50d454026f574b7b216bf388331e603769909
kbert32 <103608624+kbert32@users.noreply.github.com>
2023-08-30 21:06:46 -0600

    h2 styling

--- script.js
+++ script.js
@@ -9,23 +9,23 @@ function navToggle() {
   //   menu.classList.toggle("hidden");
   menu.classList.toggle("visible");
 }
+// emailjs.init("YKu48JfyIl8eV0_hJ");
 
-window.onload = function () {
-  emailjs.init("YKu48JfyIl8eV0_hJ");
-  document
-    .getElementById("contact-form")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
-      // generate a five digit number for the contact_number variable
-      this.contact_number.value = (Math.random() * 100000) | 0;
-      // these IDs from the previous steps
-      emailjs.sendForm("contact_service", "contact_form", this).then(
-        function () {
-          console.log("SUCCESS!");
-        },
-        function (error) {
-          console.log("FAILED...", error);
-        }
-      );
-    });
-};
+// window.onload = function () {
+//   document
+//     .getElementById("contact-form")
+//     .addEventListener("submit", function (event) {
+//       event.preventDefault();
+//       // generate a five digit number for the contact_number variable
+//       this.contact_number.value = (Math.random() * 100000) | 0;
+//       // these IDs from the previous steps
+//       emailjs.sendForm("contact_service", "contact_form", this).then(
+//         function () {
+//           console.log("SUCCESS!");
+//         },
+//         function (error) {
+//           console.log("FAILED...", error);
+//         }
+//       );
+//     });
+// };
