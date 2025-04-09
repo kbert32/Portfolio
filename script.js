cdc6d5db24919976f441f141fb08506dc66f24be
kbert32 <103608624+kbert32@users.noreply.github.com>
2025-04-08 21:04:14 -0600

    side menu changes

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
 const btn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
+const pageLinks = menu.querySelectorAll("a");
+
+// document.addEventListener("click", (event) => {
+//   if (!menu.classList.contains(".visible")) {
+//     return;
+//   }
+//   if (!event.closest("#menu")) {
+//     navToggle();
+//   }
+// });
+
+// document.addEventListener("click", closeit);
 
 btn.addEventListener("click", navToggle);
 
+// pageLinks.forEach((child) => {
+//   child.addEventListener("click", navToggle);
+// });
+
+function closeit(event) {
+  if (event.target.id === "menu" || event.target.tagName === "BUTTON") {
+    return;
+  }
+  navToggle();
+  console.log("closeit");
+  console.log(event.target.tagName);
+}
+
 function navToggle() {
+  if (menu.classList.contains("visible")) {
+    document.removeEventListener("click", closeit);
+  }
   btn.classList.toggle("open");
   menu.classList.toggle("flex");
-  //   menu.classList.toggle("hidden");
   menu.classList.toggle("visible");
+  if (menu.classList.contains("visible")) {
+    document.addEventListener("click", closeit);
+  }
+  console.log("navToggle");
 }
 
 // emailjs.init("YKu48JfyIl8eV0_hJ");
